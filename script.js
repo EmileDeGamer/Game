fe3ad13136eecae2abcfdd97b492eabcbbf9be84
EmileDeGamer <emilemolzoho@gmail.com>
2020-03-10 17:54:23 +0100

    added coords to each position

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let bord = document.getElementById('bord')
-bord.style.gridTemplateColumns = "repeat(192, 10px)"
-bord.style.gridTemplateRows = "repeat(54, 20px)"
-for (let i = 0; i <= 10367; i++) {
-    let button = document.createElement('span')
-    button.innerHTML = "0"
-    bord.appendChild(button)
+bord.style.gridTemplateColumns = "repeat(100, 15px)"
+bord.style.gridTemplateRows = "repeat(100, 15px)"
+
+let coordX = 0, coordY = 0
+
+for (let i = 0; i < 100; i++) {
+    for (let x = 0; x < 100; x++) {
+        let button = document.createElement('span')
+        button.style.backgroundColor = "white"
+        button.innerHTML = coordX + ":" + coordY
+        button.style.color = button.style.backgroundColor
+        bord.appendChild(button)
+        coordX++;
+    }
+    coordY++;
+    coordX = 0;
 }
+
+console.log(coordX + ":" + coordY)
